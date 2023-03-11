f474e16936337f5a3c76a7ec375b02e1e8f1c9de
Fran Ortega <ortegfran@gmail.com>
2023-03-11 22:19:02 +0100

    Posts.db e imagenes

---
+++ BLOG/src/app/db/posts.db.ts
@@ -0,0 +1,77 @@
+import { Post } from "../interfaces/post.interface";
+
+
+export const POSTS: Post[] = [
+    {
+        titulo: '5 Consejos para iniciarse en el Snowboard',
+        texto: '¿Empiezas con el snowboard? Estas en el sitio adecuado. Sin descubrirte nada nuevo, te listamos 5 cosas a tener en cuenta para que centres toda tu antención en aprender a surfear.',
+        autor: 'Fran Ortega',
+        imagen: '/assets/images/snowboard-113784_640.jpg',
+        fecha: '11-03-23',
+        categoria: 'Snowboard',
+        id_post: 1
+    },
+    {
+        titulo: 'Temporada 2022-2023: Apertura de pistas en España, Andorra y Francia', texto: 'El invierno está a la vuelta de la esquina. Nos parece mentira porque en muchos lugares de la península estamos por encima de los 25 grados y llevamos puestas aun las sandalias. Pero sabemos que, antes o después, terminará el veranillo de San Miguel o veroño y llegará el frío.  No sabemos si será la próxima semana o tal vez la siguiente, pero lo que si sabemos es que winter is coming.',
+        autor: 'Fran Ortega',
+        imagen: '/assets/images/galeria-cerler-81.jpg',
+        fecha: '11-03-23',
+        categoria: 'Centros de Nieve',
+        id_post: 2
+    },
+    {
+        titulo: '3 Consejos para elegir la mejor bota de snowboard',
+        texto: 'Hoy te traemos tres tips para ayudarte en la elección de tus botas de snowboard. Puede que no sean para elegir ‘la mejor’, pero si la que más te conviene según tu forma de surfear o el nivel o presupuesto que tengas. ¿Listo? ¡Vamos allá!',
+        autor: 'Fran Ortega',
+        imagen: '/assets/images/botas-snowboard-nike-tecnologia-led.jpg',
+        fecha: '11-03-23',
+        categoria: 'Material Snowboard',
+        id_post: 3
+    },
+    {
+        titulo: 'Mantenimiento de una tabla de snowboard',
+        texto: 'Para que tu tabla pueda durar mucho más tiempo del que tenías estimado aprende a saber cómo cuidarla. Queremos que estés preparado para la temporada de nieve que ya está aquí',
+        autor: 'Fran Ortega',
+        imagen: '/assets/images/snows.jpg',
+        fecha: '11-03-23',
+        categoria: 'Material Snowboard',
+        id_post: 4
+    },
+    {
+        titulo: 'Diferencias entre snowboard Freestyle y Freeride',
+        texto: '¡Si! La temporada de nieve ya está con nosotros y ser nuevo en el mundo del snow implica mucha ilusión pero también dudas, sobre todo con los términos. Seguro que te sientes confundido entre la palabra freeride o freestyle ¿Los haz probado alguna vez? Ambos tienen estilos y finalidades distintas.',
+        autor: 'Fran Ortega',
+        imagen: '/assets/images/freestyle.jpeg',
+        fecha: '11-03-23',
+        categoria: 'Snowboard',
+        id_post: 5
+    },
+    {
+        titulo: 'Snowzone Xanadu',
+        texto: 'Xanadú Snowzone ofrece posibilidades para todo tipo de públicos, desde los que se quieren iniciar en el esquí y el freestyle, familias y amigos, hasta los que quieren perfeccionar su técnica.',
+        autor: 'Fran Ortega',
+        imagen: '/assets/images/snozone-disfruta-x2.jpg',
+        fecha: '11-03-23',
+        categoria: 'Centros de Nieve',
+        id_post: 6
+    },
+    {
+        titulo: 'Hacer un backflip con snowboard. Tutorial',
+        texto: 'Te animamos a que pruebes de hacer un backflip con tu snowboard, pero te recomendamos que la primera vez sea con nieve polvo en la recepción',
+        autor: 'Fran Ortega',
+        imagen: '/assets/images/backflip.jpg',
+        fecha: '11-03-23',
+        categoria: 'Snowboard',
+        id_post: 7
+    },
+    {
+        titulo: 'Viaje Alpes, Avoriaz',
+        texto: 'Paraiso de esquiadores situado en la Alta Saboya, donde por las calles los coches son reemplazados por esquiadores y trineos de caballos. Orientada completamente a sur, Avoriaz es el centro del extensísimo dominio esquiable de Les Portes du Soleil. que es uno de los mayores dominios esquiables del mundo formado por 14 pueblos distintos, que se conectan entre ellos esquiando. ',
+        autor: 'Fran Ortega',
+        imagen: '/assets/images/avoriaz_1.jpg',
+        fecha: '11-03-23',
+        categoria: 'Centros de Nieve',
+        id_post: 8
+    },
+]
+
