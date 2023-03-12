ed44fa9c361ee1118883d63c63f1d334e9e1de4e
Fran Ortega <ortegfran@gmail.com>
2023-03-12 14:11:07 +0100

    get All posts

--- BLOG/src/app/services/post.service.ts
+++ BLOG/src/app/services/post.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { POSTS } from '../db/posts.db';
+import { Post } from '../interfaces/post.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,9 @@ import { Injectable } from '@angular/core';
 export class PostService {
 
   constructor() { }
+
+
+  getAll(): Post[] {
+    return POSTS
+  }
 }
