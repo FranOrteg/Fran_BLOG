b86b102971570af6cb3112790a512584d98be3b2
Fran Ortega <ortegfran@gmail.com>
2023-03-18 14:24:39 +0100

    Arreglo formulario

--- BLOG/src/app/components/formulario/formulario.component.ts
+++ BLOG/src/app/components/formulario/formulario.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Post } from 'src/app/interfaces/post.interface';
 import { PostService } from 'src/app/services/post.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-formulario',
@@ -10,19 +11,46 @@ import { PostService } from 'src/app/services/post.service';
 })
 export class FormularioComponent {
 
-  arrNuevoPost: Post[]
+  formulario: FormGroup
 
   constructor(
     private postService: PostService,
     private route: Router
   ) {
-    this.arrNuevoPost = []
+    this.formulario = new FormGroup({
+      titulo: new FormControl(null, [
+        Validators.required
+      ]),
+      texto: new FormControl(null, [
+        Validators.required
+      ]),
+      imagen: new FormControl(null, [
+        Validators.required
+      ]),
+      autor: new FormControl(null, [
+        Validators.required
+      ]),
+      fecha: new FormControl(null, [
+        Validators.required
+      ]),
+      categoria: new FormControl(null, [
+        Validators.required
+      ]),
+    })
   }
 
-  onSubmit(formValues: any) {
-    this.arrNuevoPost = this.postService.create(formValues)
-    console.log(formValues)
-    this.route.navigate(['/posts'])
-  }
+  onSubmit() {
+    if (!this.formulario.valid) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Tienes que rellenar todos los campos!',
+      })
+    } else {
+      this.postService.create(this.formulario.value)
+      console.log(this.formulario.value)
+      this.route.navigate(['/posts'])
+    }
 
+  }
 }
